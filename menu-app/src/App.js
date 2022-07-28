948bfe1e8ced40ffd66121f466dab05d99407512
KalynS <Kalyn@udel.edu>
2022-07-28 14:35:27 +0000

    updated css

--- menu-app/src/App.js
+++ menu-app/src/App.js
@@ -5,10 +5,13 @@ import Categories from './Components/Categories';
 import items from './Components/Data';
 import React, { useState } from "react";
 
+//only get unique categories from the list
+const allCategories = ['all',...new Set(items.map((item)=> item.category))]
+
 
 function App() {
   const[menuItems,setMenuItems] = useState(items);
-  const [categories,setCategories] = useState ([]);
+  const [categories,setCategories] = useState (allCategories);
 
   const filterItems = (category) => {
     if(category === 'all'){
@@ -21,10 +24,10 @@ function App() {
   return <main>
     <section className="menu section">
     <div className='title'>
-      <h2>Our Menu</h2>
+      <h2>Kalyn's Menu</h2>
       <div className='underline'></div>
     </div>
-    <Categories filterItems={filterItems}/>
+    <Categories categories={categories} filterItems={filterItems}/>
     <Menu items={menuItems}/>
     </section>
 
